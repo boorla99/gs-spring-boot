2a78a06c2ec98a4360000e38f8ba5ebbba175004
Pradeep Kumar <pradeep.kumar@scotiabank.com>
2021-07-04 02:12:13 -0400

    add update

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Hello BNS TEAM Greetings from Spring Boot!";
     }   
 }
